ce5391940f8c949596ca87279d2b7ce071f89b12
Emily Caggiano <emilycaggiano@Emilys-MacBook-Pro-60.local>
2018-10-01 11:39:44 -0400

    documented othello.cc file

--- othello.cc
+++ othello.cc
@@ -8,6 +8,10 @@
 namespace main_savitch_14
 {
 
+/**This function sets up the board display to have 8 columns and 8 rows with blue dividing lines
+and two black pieces and two white pieces in the starting postitions.
+It also outputs the prompt for the user to enter their move with a letter and a number**/
+
 void Othello::display_status()const
 {
   cout << BLUE << "   _______ _______ _______ _______ _______ _______ _______ _______\n";
@@ -47,6 +51,10 @@ void Othello::display_status()const
   }
   cout << "\nEnter a move in the format(letter,number) (Ex: A3)" << endl;
 }
+
+/**This function uses determines what color piece to place on the board, and how it will affect the other pieces 
+around it. It also increments the move number by one to keep track of the number of moves that have been made 
+by each player.**/
 
 void Othello::make_move(const string& move) {
 
@@ -189,7 +197,10 @@ void Othello::make_move(const string& move) {
   move_number++;
 }
 
-
+/**This function resets the game and the board. It places two white pieces and two black pieces on the board in the 
+appropriate position. It also sets the number of skips back to zero, the number of open spots to 60, and the number
+of black and white pieces to 2.**/
+ 
 void Othello::restart() {
 
   gameBoard[3][3].set_white();
@@ -202,6 +213,10 @@ void Othello::restart() {
   skips = 0;
   openSpots = 60;
 }
+
+/**This function checks to make sure that the move input by the player is legal. It checks if the board space is empty,
+and also if the surrounding pieces are black or white. Based on these parameter, it returns a bool true or false
+to determine if the move is legal or illegal.**/
 
 bool Othello::is_legal(const string& move)const {
 
@@ -380,6 +395,8 @@ bool Othello::is_legal(const string& move)const {
   }
 }
 
+/**this function counts the number of black and white pieces currently on the board. It also displays the total number
+of moves that have been completed, the number of open spots, and the number of black and white pieces on the board.**/ 
 void Othello::countingPieces() {
 
   int black = 0;
@@ -400,6 +417,10 @@ void Othello::countingPieces() {
   cout << endl;
 }
 
+/**This function determines if the game is over by determining if the number of skips is equal to two, if there are no 
+open spots, or the number of the opposing pieces on the board is equal to zero. It then alerts the user that the game 
+is over**/
+
 bool Othello::is_game_over()const {
 
   if (skips == 2 || openSpots == 0 || black == 0 || white == 0) {
@@ -411,6 +432,9 @@ bool Othello::is_game_over()const {
   }
 }
 
+/**This function determines whos turn it is, based on who made the previous move. It then outputs this to the screen
+to let the user know that it is their turn**/
+
 void Othello::whosTurn() {
   if (next_mover() == COMPUTER) {
     cout << "\nWhite's Turn!" << endl;
@@ -421,9 +445,14 @@ void Othello::whosTurn() {
   }
 }
 
+/**This function counts the number of skips that have been done by the user**/
+
 void Othello::make_skips() {
   skips++;
 }
+
+/**This function determines who is currently winning the game by determining if the number of white or black pieces is 
+greater, and determining who is winning, or if it is a tie**/
 
 game::who Othello::winning()const {
 
@@ -436,6 +465,8 @@ game::who Othello::winning()const {
   }
 
 }
+
+/**This function evaluates the best move for the computer to do from the computed moves that it can make**/
 
 int Othello::evaluate()const {
 
@@ -456,6 +487,7 @@ int Othello::evaluate()const {
   }
 }
 
+/**This function computes the moves that are possible for the computer to make**/
 void Othello::compute_moves(std::queue<std::string>& moves)const {
   string r;
   string c;
